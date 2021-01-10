f27c7a25c0de33d15d26e8b7e9385a12091e2b4c
JasonAkiraJordan <jasonajordan0@gmail.com>
2021-01-10 16:14:16 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import Home from "./Home";
+import About from "./About";
+import NavBar from "./NavBar";
 
-function App() {
+function App() {  
   return (
     <div>
       <NavBar />
